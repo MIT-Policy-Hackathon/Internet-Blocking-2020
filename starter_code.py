d696576825ce1e7d67f6b5ab44f895928454f36c
Andreas Haupt <haupt@mit.edu>
2020-10-24 11:36:55 -0400

    Allow to read-in web-connectivity probes

--- starter_code.py
+++ starter_code.py
@@ -1,9 +1,20 @@
 import os
 import json
+
+FILE_PATH = "."
+
 iran_measurements = {}
-for filename in os.listdir(".")[:10]:
+for filename in os.listdir(FILE_PATH)[:100]:
     with open(filename, 'r') as infile:
-        try:
-            iran_measurements[filename] = json.load(infile)
-        except Exception as e:
-            print(line, e)
+        lines = infile.readlines()
+        if len(lines) > 1:
+            # This is a web connectivity probe, make a list
+            iran_measurements[filename] = [json.loads(line) for line in lines]
+        elif len(lines) > 0:
+            # This is another probe
+            iran_measurements[filename] = json.loads(lines[0])
+        else:
+            print("Cannot read file {}".format(filename))
+            
+# Example exploration
+list(iran_measurements.values())[0]['test_keys']
